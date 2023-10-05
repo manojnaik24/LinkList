bfa88cbd4c3a8a0ab1b2690ed6e78aad11082428
Manoj naik <chittinaik@gamil.com>
2023-10-05 10:10:54 +0530

    [manoj] add:Add three Element

---
+++ LinkListDemo/LinkListDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using LinkListDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            linkedList l=new linkedList();
+            l.Add(56);
+            l.Add(60);
+            l.Add(70);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LinkListDemo/LinkListDemo/linkedList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkListDemo
+{
+    //Likedlist is Linar data structure
+    //It consists of groups of nodes in a sequence
+    //one is data part and address part
+    //data part= each node of a linkedlist can store a data
+    //address=it ll store the address for other node
+    internal class linkedList
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+               Node temp= head;
+                while(temp.next != null)
+                {
+                    temp= temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine(data);
+
+        }
+    }
+}
